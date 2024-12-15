2e167dcb91c2919814f628905531c187d89cd5c3
Hiếu Huỳnh <mike.beplus@gmail.com>
2024-12-15 19:31:02 +0700

    Update

--- a1a-wp-helpers.php
+++ a1a-wp-helpers.php
@@ -43,41 +43,7 @@ require_once(A1AWH_DIR . '/inc/template-tags.php');
  * @see https://developer.wordpress.org/reference/functions/register_block_type/
  */
 function create_block_a1a_wp_helpers_block_init() {
-	// register_block_type( __DIR__ . '/build' );
-	// register_block_type( __DIR__ . '/build/rating-block' );
-	// register_block_type( __DIR__ . '/build/review-card-block' ); 
 	register_block_type( __DIR__ . '/build/table-of-content' ); 
 	register_block_type( __DIR__ . '/build/membership' ); 
-	// register_block_type( __DIR__ . '/build/global-js-css' ); 
 }
 add_action( 'init', 'create_block_a1a_wp_helpers_block_init' );
-
-// Add some post meta
-// function register_review_rating_post_meta() {
-// 	$post_meta = array(
-// 		'_rating'      => array( 'type' => 'integer'	),
-// 		'_ratingStyle' => array( 'type' => 'string'	),
-// 	);
-
-// 	foreach ( $post_meta as $meta_key => $args ) {
-// 		register_post_meta(
-// 			'post',
-// 			$meta_key,
-// 			array(
-// 				'show_in_rest'  => true,
-// 				'single'        => true,
-// 				'type'          => $args['type'],
-// 				'auth_callback' => function() {
-// 					return current_user_can( 'edit_posts' );
-// 				}
-// 			)
-// 		);
-// 	}
-// }
-// add_action( 'init', 'register_review_rating_post_meta' ); 
-
-// add_action( 'wp_head', function() {
-// 	echo 'dev';
-// 	echo get_post_meta( 82, '_rating', true );
-// 	echo get_post_meta( 82, '_ratingStyle', true );
-// } );
--- inc/static.php
+++ inc/static.php
@@ -6,6 +6,5 @@
 add_action( 'wp_enqueue_scripts', 'a1awh_enqueue_scripts' );
 
 function a1awh_enqueue_scripts() {
-  // wp_enqueue_style( 'a1awh-css', A1AWH_URL . '/build/global-js-css/style-index.css', false, A1AWH_VERSION );
-  wp_enqueue_script( 'a1awh-js', A1AWH_URL . '/build/index.js', ['jquery', 'react', 'wp-element'], A1AWH_VERSION, true );
+  
 }
